54c54de5cf74c3eb94fe41e7f30e53354ddf72d9
Andrew Lee <u1256713@utah.edu>
2022-10-07 19:33:58 +0000

    initial commit

---
+++ src/components/Cross.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import ReactFlow from 'react-flow-renderer';
+
+export function Cross(){
+    return (
+      <div className="cross">
+
+      </div>  
+    );
+}
