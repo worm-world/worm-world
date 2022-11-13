ec27ad087f63e2a2c0174b326a70fcbdf17dedea
Daniel <63615764+danielb3-byte@users.noreply.github.com>
2022-11-13 13:41:42 -0700

    Sidebar for navigation, with styling. Note that the routing in App.tsx was adjusted to create nested routes. A layout component keeps arranges the main components--navbar and page.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import {Link} from 'react-router-dom'
+import Paths from '../routes/frontend';
+
+function Navbar(): JSX.Element {
+  return (
+      <nav id='navbar'>
+        <div id='title'>WormWorld</div>
+        <div id='nav-items'>
+          <Link to={Paths.CrossDesignerPath}>
+            <div className='nav-item'>Designer</div>
+          </Link>
+          <Link to={Paths.TodoPath}>
+            <div className='nav-item'>Scheduler</div>
+          </Link>
+          <Link to={Paths.ImportPath}>
+            <div className='nav-item'>Data Manager</div>
+          </Link>
+        </div>
+      </nav>
+  );
+};
+
+export default Navbar;
