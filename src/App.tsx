a96aadc62ea1fbd8f4f377414fab3a4338e1d074
Daniel <63615764+danielb3-byte@users.noreply.github.com>
2022-11-15 16:58:02 -0700

    feat: using Material UI component for sidenav

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,21 @@ import Import from './pages/import';
 import Home from './pages/home';
 import CrossPage from './pages/crossPage';
 import Paths from './routes/frontend';
-import Layout from './layout'
+import Layout from './components/layout/layout';
 
 function App(): JSX.Element {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path={Paths.CrossDesignerPath} element={<CrossPage />} />
-            <Route path={Paths.HomePath} element={<Home />} />
-            <Route path={Paths.TodoPath} element={<Todo />} />
-            <Route path={Paths.ImportPath} element={<Import />} />
-          </Route>
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path={Paths.CrossDesignerPath} element={<CrossPage />} />
+          <Route path={Paths.HomePath} element={<Home />} />
+          <Route path={Paths.TodoPath} element={<Todo />} />
+          <Route path={Paths.ImportPath} element={<Import />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
