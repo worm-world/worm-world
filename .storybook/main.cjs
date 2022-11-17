b22996cb60d3117d45841957a660b7ae7e4d19f4
Seth Gunsay <sgunsay1@gmail.com>
2022-11-17 15:29:40 -0700

    fix: storybook vulnerabilities

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,19 +1,15 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "storybook-addon-react-router-v6"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "storybook-addon-react-router-v6"],
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
   },
+  "core": {},
   "features": {
     "storyStoreV7": true
+  },
+  docsPage: {
+    docs: "automatic"
   }
-}
+};
