eb9adc9c798722ae9b5e3bd1eb05f511132401b2
Seth Gunsay <sgunsay1@gmail.com>
2022-11-17 12:03:20 -0700

    feat: add sample stories for layout and sideNav

---
+++ src/components/layout/layout.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Layout from './layout';
+
+export default {
+  title: 'Components/Layout',
+  component: Layout,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Layout>;
+
+const Template: ComponentStory<typeof Layout> = () => <Layout />;
+
+export const Primary = Template.bind({});
