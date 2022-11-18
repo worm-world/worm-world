5c1dee4f047f40b5bbefb1599b517145ff07b141
willandtommine <willandtommine@gmail.com>
2022-11-18 13:04:55 -0700

    chore: add another test

--- src/components/TopNav/TopNav.test.tsx
+++ src/components/TopNav/TopNav.test.tsx
@@ -1,25 +1,34 @@
 import { Tab } from '@mui/material';
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { TopNav } from './TopNav';
 
-const renderComponent = ({ title = 'test' }): void => {
+const renderComponent = ({ title = 'test', tabIndex = 0 }): void => {
+  const tabNames = ['tab1', 'tab2', 'tab3'];
+  const children = tabNames.map((name) => <Tab key={name} label={name} />);
+
   render(
     <TopNav title={title}>
-      {' '}
-      <Tab />{' '}
-    </TopNav>,
-    {
-      wrapper: BrowserRouter, // Need this wrapper since the component uses the react router
-    }
+      {children} tabIndex={tabIndex}
+    </TopNav>
   );
 };
 
 describe('TopNav', () => {
   test('renders topNav component', () => {
     renderComponent({});
-    screen.getByRole('tab'); // Throws an error if the element can't be found
     const tabs = screen.getAllByRole('tab');
-    expect(tabs.length).toBeGreaterThan(0);
+    expect(tabs.length).toBe(3);
+
+    expect(screen.getByText('test')).toBeValid();
+  });
+
+  test('clicking tabs work correctly', () => {
+    renderComponent({});
+    const tabs = screen.getAllByRole('tab');
+    expect(tabs[0].getAttribute('aria-selected')).toBe('true');
+
+    fireEvent.click(tabs[1]);
+    const tabsAfter = screen.getAllByRole('tab');
+    expect(tabsAfter[1].getAttribute('aria-selected')).toBe('true');
   });
 });
