1d9c7e0c9f5c91c7393464f7a2c0a60e20aa4e52
Andrew Lee <u1256713@utah.edu>
2022-11-18 17:23:03 -0700

    fix(backend): comment out allele interface stubs for now to get rid of warnings

--- src-tauri/src/interface/allele.rs
+++ src-tauri/src/interface/allele.rs
@@ -1,37 +1,37 @@
-use crate::models::{
-    allele::Allele
-};
-use super::{InnerDbState, DbError};
-use anyhow::Result;
+// use crate::models::{
+//     allele::Allele
+// };
+// use super::{InnerDbState, DbError};
+// use anyhow::Result;
 
-impl InnerDbState {
-    pub async fn get_alleles(&self) -> Result<Vec<Allele>, DbError> {
-        Err(DbError::SqlQueryError("Allele unimplememented".to_string()))
-    }
-    pub async fn insert_allele(&self, allele: &Allele) -> Result<(), DbError> {
-        Err(DbError::SqlInsertError("Allele unimplemented".to_string()))
-    }
-}
+// impl InnerDbState {
+//     pub async fn get_alleles(&self) -> Result<Vec<Allele>, DbError> {
+//         Err(DbError::SqlQueryError("Allele unimplememented".to_string()))
+//     }
+//     pub async fn insert_allele(&self, allele: &Allele) -> Result<(), DbError> {
+//         Err(DbError::SqlInsertError("Allele unimplemented".to_string()))
+//     }
+// }
 
-#[cfg(test)]
-mod test {
-    use crate::dummy::testdata;
-    use crate::models::allele::Allele;
-    use crate::InnerDbState;
-    use anyhow::Result;
-    use sqlx::{Pool, Sqlite};
-    use pretty_assertions::{assert_eq};
+// #[cfg(test)]
+// mod test {
+//     use crate::dummy::testdata;
+//     use crate::models::allele::Allele;
+//     use crate::InnerDbState;
+//     use anyhow::Result;
+//     use sqlx::{Pool, Sqlite};
+//     use pretty_assertions::{assert_eq};
 
-    #[sqlx::test(fixtures("dummy"))]
-    async fn test_get_alleles(pool: Pool<Sqlite>) -> Result<()> {
-        let state = InnerDbState { conn_pool: pool };
-        // TODO: implement
-        Ok(())
-    }
-    #[sqlx::test(fixtures("dummy"))]
-    async fn test_insert_allele(pool: Pool<Sqlite>) -> Result<()> {
-        let state = InnerDbState { conn_pool: pool };
-        // TODO: implement
-        Ok(())
-    }
-}
+//     #[sqlx::test(fixtures("dummy"))]
+//     async fn test_get_alleles(pool: Pool<Sqlite>) -> Result<()> {
+//         let state = InnerDbState { conn_pool: pool };
+//         // TODO: implement
+//         Ok(())
+//     }
+//     #[sqlx::test(fixtures("dummy"))]
+//     async fn test_insert_allele(pool: Pool<Sqlite>) -> Result<()> {
+//         let state = InnerDbState { conn_pool: pool };
+//         // TODO: implement
+//         Ok(())
+//     }
+// }
