7c24a62433d81cb0becef0ba6ebee35555ed3f38
Daniel <63615764+danielb3-byte@users.noreply.github.com>
2022-12-03 15:12:24 -0700

    feat: right drawer with adjustable width, taking children

---
+++ src/components/rightDrawer/RightDrawer.stories.tsx
@@ -0,0 +1,29 @@
+import { StoryFn, Meta } from '@storybook/react';
+import RightDrawer from './RightDrawer';
+import React from 'react';
+
+export default {
+  title: 'Components/RightDrawer',
+  component: RightDrawer,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta<typeof RightDrawer>;
+
+const Template: StoryFn<typeof RightDrawer> = (args) => {
+  args.close = () => alert('close button pressed');
+  return <RightDrawer {...args} />;
+}
+export const Primary = Template.bind({});
+Primary.args = {
+  initialDrawerWidth: 100,
+  maxWidth: 400,
+}
+
+export const WithChildren = Template.bind({});
+WithChildren.args = {
+  initialDrawerWidth: 100,
+  maxWidth: 400,
+  isOpen: true,
+  children: <div>Children here</div>,
+}
---
+++ src/components/rightDrawer/RightDrawer.tsx
@@ -0,0 +1,94 @@
+import { Drawer, Box, IconButton } from '@mui/material';
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
+import styles from './RightDrawer.module.css';
+import React, { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+
+export interface RightDrawerProps {
+  initialDrawerWidth: number;
+  maxWidth: number;
+  isOpen: boolean;
+  close: () => void;
+  backgroundColor?: string;
+  children?: JSX.Element | JSX.Element[];
+}
+
+const RightDrawer = (props: RightDrawerProps): ReactJSXElement => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [drawerWidth, setDrawerWidth] = useState(props.initialDrawerWidth);
+  const [mousePosition, setMousePosition] = React.useState<{
+    x: number;
+    y: number;
+  }>({ x: 0, y: 0 });
+
+  // Draggable
+  React.useEffect(() => {
+    const calculateNewWidth = (ev: MouseEvent): void => {
+      const widthChange = mousePosition.x - ev.clientX;
+      const newWidth = drawerWidth + widthChange;
+      if (isDragging) {
+        setDrawerWidth(newWidth);
+      }
+      setMousePosition({ x: ev.clientX, y: ev.clientY });
+    };
+
+    const endDragging = (): void => setIsDragging(false);
+
+    window.addEventListener('mousemove', calculateNewWidth);
+    window.addEventListener('mouseup', endDragging);
+
+    return () => {
+      window.removeEventListener('mousemove', calculateNewWidth);
+      window.removeEventListener('mouseup', endDragging);
+    };
+  }, [isDragging]);
+
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+          display: 'flex',
+          flexDirection: 'row',
+          minWidth: 'fit-content',
+          maxWidth: props.maxWidth,
+          backgroundColor: props.backgroundColor,
+        },
+      }}
+      variant='persistent'
+      anchor='right'
+      open={props.isOpen}
+    >
+      <Box
+        className={styles.leftEdge}
+        style={{
+          borderLeft: isDragging ? '3px solid #2196f3' : 'none',
+          cursor: 'col-resize',
+        }}
+        onMouseDown={() => setIsDragging(true)}
+      />
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <IconButton
+            onClick={() => props.close()}
+            sx={{ border: 'none', boxShadow: 'none' }}
+          >
+            <CloseIcon></CloseIcon>
+          </IconButton>
+        </Box>
+        <Box className={styles.drawerContents}>{props.children}</Box>
+      </Box>
+    </Drawer>
+  );
+};
+
+export default RightDrawer;
