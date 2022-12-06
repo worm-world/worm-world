a6846de12fa5d3c94b9bfacee6ee60a7b923d80b
Seth Gunsay <sgunsay1@gmail.com>
2022-12-05 22:25:40 -0700

    feat: update home page with example buttons creating types

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getGene } from 'api/gene';
+import { getGenes } from 'api/gene';
 import { TopNav } from 'components/TopNav/TopNav';
 import { getAlleles } from 'api/allele';
 import { getAlleleExpressions } from 'api/alleleExpressions';
@@ -7,6 +7,11 @@ import { getAlteringConditions, getConditions } from 'api/condition';
 import { getAlteringPhenotypes, getPhenotypes } from 'api/phenotype';
 import { getVariations } from 'api/variationInfo';
 import { Gene } from 'models/frontend/Gene';
+import { Allele } from 'models/frontend/Allele';
+import { AlleleExpression } from 'models/frontend/AlleleExpression';
+import { Condition } from 'models/frontend/Condition';
+import { Phenotype } from 'models/frontend/Phenotype';
+import { VariationInfo } from 'models/frontend/VariationInfo';
 
 const Home = (): JSX.Element => {
   return (
@@ -14,68 +19,95 @@ const Home = (): JSX.Element => {
       <TopNav title={'Home'} />
       <button
         onClick={() => {
-          getGene('unc-119')
+          getGenes()
             .then((res) => {
-              if (res != null) {
-                const gene = Gene.createFromRecord(res);
-
-                console.log(gene);
-              }
+              const genes = res.map((record) => Gene.createFromRecord(record));
+              console.log(genes);
             })
             .catch((err) => err);
         }}
       >
-        get gene
+        get genes
       </button>
       <button
         onClick={() => {
           getAlleles()
-            .then((res) => console.log(res))
+            .then((res) => {
+              const alleles = res.map((record) =>
+                Allele.createFromRecord(record)
+              );
+              console.log(alleles);
+            })
             .catch((err) => err);
         }}
       >
-        get allele
+        get alleles
       </button>
       <button
         onClick={() => {
           getAlleleExpressions()
-            .then((res) => console.log(res))
+            .then((res) => {
+              const alleleExpressions = res.map((record) =>
+                AlleleExpression.createFromRecord(record)
+              );
+              console.log(alleleExpressions);
+            })
             .catch((err) => err);
         }}
       >
-        get Allele expression
+        get Allele expressions
       </button>
       <button
         onClick={() => {
           getConditions()
-            .then((res) => console.log(res))
+            .then((res) => {
+              const conditions = res.map((record) =>
+                Condition.createFromRecord(record)
+              );
+              console.log(conditions);
+            })
             .catch((err) => err);
         }}
       >
-        get condition
+        get conditions
       </button>
       <button
         onClick={() => {
           getPhenotypes()
-            .then((res) => console.log(res))
+            .then((res) => {
+              const phenotypes = res.map((record) =>
+                Phenotype.createFromRecord(record)
+              );
+              console.log(phenotypes);
+            })
             .catch((err) => err);
         }}
       >
-        get phenotype
+        get phenotypes
       </button>
       <button
         onClick={() => {
           getVariations()
-            .then((res) => console.log(res))
+            .then((res) => {
+              const varations = res.map((record) =>
+                VariationInfo.createFromRecord(record)
+              );
+              console.log(varations);
+            })
             .catch((err) => err);
         }}
       >
-        get variation
+        get variations
       </button>
       <button
         onClick={() => {
           getAlteringPhenotypes('oxIs644', 'YFP(pharynx)', false, false)
-            .then((res) => console.log(res))
+            .then((res) => {
+              const alteringPhenotypes = res.map((record) =>
+                Phenotype.createFromRecord(record)
+              );
+              console.log(alteringPhenotypes);
+            })
             .catch((err) => err);
         }}
       >
@@ -84,7 +116,12 @@ const Home = (): JSX.Element => {
       <button
         onClick={() => {
           getAlteringConditions('n765', 'lin-15B', false, false)
-            .then((res) => console.log(res))
+            .then((res) => {
+              const alteringConditions = res.map((record) =>
+                Condition.createFromRecord(record)
+              );
+              console.log(alteringConditions);
+            })
             .catch((err) => err);
         }}
       >
