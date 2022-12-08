8715598d14e381a296c2ced4eca322156a9c12ba
Seth Gunsay <sgunsay1@gmail.com>
2022-12-08 13:32:56 -0700

    feat: templates for data insert/display

---
+++ src/pages/data-import/insert-allele-expression.tsx
@@ -0,0 +1,11 @@
+const InsertAlleleExpression = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Testing Allele Expression</h1>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default InsertAlleleExpression;
---
+++ src/pages/data-import/insert-gene.tsx
@@ -0,0 +1,10 @@
+const InsertGene = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Testing Gene</h1>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+export default InsertGene;
