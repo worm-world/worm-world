74fd77e5eaf840b3e68d534fd1784ce1d740267e
Andrew Lee <u1256713@utah.edu>
2022-12-07 17:43:54 -0700

    refactor(frontend): remove dead/unnecessary code

--- src/components/layout/layout.test.tsx
+++ src/components/layout/layout.test.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const renderComponent = (): void => {
   render(<Layout>{}</Layout>, {
-    wrapper: BrowserRouter
+    wrapper: BrowserRouter,
   });
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, FC} from 'react';
+import React, { Suspense, FC } from 'react';
 import ReactDOM from 'react-dom/client';
 import 'styles/global.css';
 import Layout from 'components/layout/layout';
@@ -6,10 +6,7 @@ import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
 
 import routes from '~react-pages';
 
-// eslint-disable-next-line no-console
-console.log(routes);
-
-const App : FC = () => {
+const App: FC = () => {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <main>
