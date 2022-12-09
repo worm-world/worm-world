2bc4b7121ea7487c85f32790df0c0a58494d294d
Andrew Lee <u1256713@utah.edu>
2022-12-08 20:29:58 -0700

    fix(data-manager): fix then refactor Outlet command

--- src/pages/data-manager.tsx
+++ src/pages/data-manager.tsx
@@ -1,10 +1,9 @@
 import { Tab } from '@mui/material';
 import { TopNav } from 'components/TopNav/TopNav';
-import { Link, useOutlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import 'styles/data-manager.css';
 
 const Import = (): JSX.Element => {
-  const outlet = useOutlet();
   return (
     <div>
       <TopNav title={'Data Manager'} tabIndex={false}>
@@ -24,7 +23,7 @@ const Import = (): JSX.Element => {
           to='expression-relation'
         />
       </TopNav>
-      {outlet != null || <h1 className='p-4 text-2xl'>Select a data tab.</h1>}
+      <Outlet />
     </div>
   );
 };
---
+++ src/pages/data-manager/index.tsx
@@ -0,0 +1,4 @@
+const DataManager = (): JSX.Element => {
+  return <h1 className='p-12 text-3xl'>Select a data tab.</h1>;
+};
+export default DataManager;
