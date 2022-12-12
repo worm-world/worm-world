716511e7c5d4d8f6b701de2b4f9008b09a45d2e2
Andrew Lee <u1256713@utah.edu>
2022-12-12 00:32:13 -0700

    fix(crossNode): wrap with react flow provider

--- src/components/XNode/XNode.tsx
+++ src/components/XNode/XNode.tsx
@@ -5,7 +5,7 @@ import { Handle, Position } from 'reactflow';
 
 export const XNode = (): ReactJSXElement => {
   return (
-    <div className='rounded-full border-solid shadow bg-gray-50 hover:bg-gray-100 h-16 w-16'>
+    <div className='rounded-full border-2 shadow bg-white hover:bg-gray-100 h-16 w-16'>
       <Handle type='target' position={Position.Top} />
       <div className='flex justify-center items-center h-full'>
         <CloseIcon fontSize='large'></CloseIcon>
