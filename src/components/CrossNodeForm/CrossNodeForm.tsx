ef4d9306c6fddfa158eb978fc9a513a9f81ad92e
Daniel <63615764+danielb3-byte@users.noreply.github.com>
2023-01-19 11:32:34 -0700

    feat: integrated adding crossnode function, along with frontend-filtering of options

--- src/components/CrossNodeForm/CrossNodeForm.tsx
+++ src/components/CrossNodeForm/CrossNodeForm.tsx
@@ -1,17 +1,17 @@
 import { getFilteredGenes, getGenes } from 'api/gene';
 import { Gene } from 'models/frontend/Gene/Gene';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { VariationInfo } from 'models/frontend/VariationInfo/VariationInfo';
-import { getFilteredVariations, getVariations } from 'api/variationInfo';
+import { getFilteredVariations } from 'api/variationInfo';
 import { Allele } from 'models/frontend/Allele/Allele';
 import { getFilteredAlleles } from 'api/allele';
 import CrossNodeModel from 'models/frontend/CrossNode/CrossNode';
 import { Sex } from 'models/enums';
 import CrossNode from 'components/CrossNode/CrossNode';
-import { Multiselector, Selector, Option } from 'components/Selector/Selector';
-import { Filter, FilterTuple } from 'models/db/filter/Filter';
-import { AlleleFieldName } from 'models/db/filter/db_AlleleFieldName';
 import { DynamicMultiSelect } from 'components/Selector/DynamicMultiSelect';
+import { db_Gene } from 'models/db/db_Gene';
+import { db_VariationInfo } from 'models/db/db_VariationInfo';
+import { db_Allele } from 'models/db/db_Allele';
 
 export interface CrossNodeFormProps {
   addNewCrossNode: (arg: JSX.Element) => void;
@@ -19,91 +19,72 @@ export interface CrossNodeFormProps {
 
 // CrossNode form specifies a new cross node
 const CrossNodeForm = (props: CrossNodeFormProps): JSX.Element => {
-  // Set up options of the form {label: string, value: T}
-  const [geneOptions, setGeneOptions] = useState<Array<Option<Gene>>>([]);
-  const [variationOptions, setVariationOptions] = useState<
-    Array<Option<VariationInfo>>
-  >([]);
-  const [alleleOptions, setAlleleOptions] = useState<Array<Option<Allele>>>([]);
-
   // Save selected options
-  const [selectedSex, setSelectedSex] = useState<Option<Sex>>({
-    label: 'male',
-    value: Sex.Male,
-  });
-  const [selectedGenes, setSelectedGenes] = useState<Array<Option<Gene>>>([]);
+  const [selectedSex, setSelectedSex] = useState<Sex>(Sex.Male);
+  const [selectedGenes, setSelectedGenes] = useState<Set<db_Gene>>(new Set());
   const [selectedVariations, setSelectedVariations] = useState<
-    Array<Option<VariationInfo>>
-  >([]);
-  const [selectedAlleles, setSelectedAlleles] = useState<Array<Option<Allele>>>(
-    []
-  );
+    Set<db_VariationInfo>
+  >(new Set());
+  const [selectedFemaleAlleles, setSelectedFemaleAlleles] = useState<
+    Set<db_Allele>
+  >(new Set());
+  const [selectedMaleAlleles, setSelectedMaleAlleles] = useState<
+    Set<db_Allele>
+  >(new Set());
 
-  // Populate genes, variations, and (filtered) alleles multiselects
+  // Only allow existence of selected alleles consistent with other selections
   useEffect(() => {
-    populateGeneOptions(setGeneOptions);
-  }, []);
-  useEffect(() => {
-    populateVariationOptions(setVariationOptions);
-  }, []);
-  useEffect(() => {
-    setSelectedAlleles([]);
-    // Don't repopulate if no genes or variations selected
-    if (selectedGenes.length === 0 && selectedVariations.length === 0) {
-      setAlleleOptions([]);
-    } else {
-      refreshAlleleOptions(
-        selectedGenes,
-        selectedVariations,
-        setAlleleOptions
-      ).catch((e) => {
-        console.error(e);
-      });
-    }
-  }, [selectedGenes, selectedVariations]);
+    setSelectedMaleAlleles(new Set());
+    setSelectedFemaleAlleles(new Set());
+  }, [selectedSex, selectedGenes, selectedVariations]);
 
-  // Selectors
-
-  // Submission handler
-  const sexForNode: Sex = selectedSex.value;
-  const genesForNode: Gene[] = [];
-  selectedGenes.forEach((selectedGene) => {
-    genesForNode.push(selectedGene.value);
-  });
-  const variationsForNode: VariationInfo[] = [];
-  selectedVariations.forEach((selectedVariation) => {
-    variationsForNode.push(selectedVariation.value);
-  });
-  const allelesForNode: Allele[] = [];
-  selectedAlleles.forEach((selectedAllele) => {
-    allelesForNode.push(selectedAllele.value);
-  });
   const onSubmit = (): void => {
-    const newNode = createNewCrossNode(
-      sexForNode,
-      genesForNode,
-      variationsForNode,
-      allelesForNode
+    const sexForNode: Sex = selectedSex;
+    const genesForNode: Gene[] = Array.from(selectedGenes).map(
+      (selectedGene) => {
+        return Gene.createFromRecord(selectedGene);
+      }
     );
-    props.addNewCrossNode(newNode);
+    const variationsForNode: VariationInfo[] = Array.from(
+      selectedVariations
+    ).map((selectedVariation) => {
+      return VariationInfo.createFromRecord(selectedVariation);
+    });
+    const maleAllelePromisesForNode: Promise<Allele>[] = Array.from(
+      selectedMaleAlleles
+    ).map((selectedAllele) => {
+      return Allele.createFromRecord(selectedAllele);
+    });
+    const femaleAllelePromisesForNode: Promise<Allele>[] = Array.from(
+      selectedFemaleAlleles
+    ).map((selectedAllele) => {
+      return Allele.createFromRecord(selectedAllele);
+    });
+    const allAlleles = maleAllelePromisesForNode.concat(
+      femaleAllelePromisesForNode
+    );
+    Promise.all(allAlleles).then((allelesForNode) => {
+      const newNode = createNewCrossNode(
+        sexForNode,
+        genesForNode,
+        variationsForNode,
+        allelesForNode
+      );
+      props.addNewCrossNode(newNode);
+    });
   };
 
   return (
     <>
-      <h3 className='font-medium'>Add a New Cross Node</h3>
-      <label className='label'>
-        <span className='label-text'>Sex</span>
-      </label>
-      <select className='select-bordered select w-full max-w-xs'>
-        <option value={Sex.Male}>male</option>
-        <option value={Sex.Hermaphrodite}>hermaphrodite</option>
-      </select>
+      <SexSelector setSelectedSex={setSelectedSex} />
       <DynamicMultiSelect
         getFilteredRecordApi={getFilteredGenes}
         searchOn={'SysName'}
         selectInputOn={'sysName'}
         displayResultsOn={['sysName']}
         label='Genes to Display'
+        selectedRecords={selectedGenes}
+        setSelectedRecords={setSelectedGenes}
       />
       <DynamicMultiSelect
         getFilteredRecordApi={getFilteredVariations}
@@ -111,13 +92,44 @@ const CrossNodeForm = (props: CrossNodeFormProps): JSX.Element => {
         selectInputOn={'alleleName'}
         displayResultsOn={['alleleName']}
         label='Variations to Display'
+        selectedRecords={selectedVariations}
+        setSelectedRecords={setSelectedVariations}
       />
       <DynamicMultiSelect
         getFilteredRecordApi={getFilteredAlleles}
         searchOn={'Name'}
         selectInputOn={'name'}
         displayResultsOn={['name']}
-        label='Alleles'
+        label='Male Alleles'
+        selectedRecords={selectedMaleAlleles}
+        setSelectedRecords={setSelectedMaleAlleles}
+        // Only include alleles of selected genes or variations
+        shouldInclude={(dbAllele) =>
+          shouldIncludeDbAllele(
+            selectedGenes,
+            selectedVariations,
+            selectedMaleAlleles,
+            dbAllele
+          )
+        }
+      />
+      <DynamicMultiSelect
+        getFilteredRecordApi={getFilteredAlleles}
+        searchOn={'Name'}
+        selectInputOn={'name'}
+        displayResultsOn={['name']}
+        label='Female Alleles'
+        selectedRecords={selectedFemaleAlleles}
+        setSelectedRecords={setSelectedFemaleAlleles}
+        // Only include alleles of selected genes or variations
+        shouldInclude={(dbAllele) =>
+          shouldIncludeDbAllele(
+            selectedGenes,
+            selectedVariations,
+            selectedFemaleAlleles,
+            dbAllele
+          )
+        }
       />
       <button
         className='btn mt-5 max-w-xs bg-accent'
@@ -131,78 +143,52 @@ const CrossNodeForm = (props: CrossNodeFormProps): JSX.Element => {
   );
 };
 
-const populateGeneOptions = (
-  setGeneOptions: React.Dispatch<React.SetStateAction<Array<Option<Gene>>>>
-): void => {
-  getGenes()
-    .then((geneRecords) => {
-      const genes = geneRecords.map((record) => Gene.createFromRecord(record));
-      const geneOptions: Array<Option<Gene>> = [];
-      for (const gene of genes) {
-        geneOptions.push({ value: gene, label: gene.descName ?? gene.sysName });
-      }
-      setGeneOptions(geneOptions);
-    })
-    .catch((e) => {
-      console.error(e);
-      setGeneOptions([]);
-    });
+const shouldIncludeDbAllele = (
+  selectedGenes: Set<db_Gene>,
+  selectedVariations: Set<db_VariationInfo>,
+  selectedAlleles: Set<db_Allele>,
+  allele: db_Allele
+) => {
+  const alleleOfGene =
+    (allele.sysGeneName &&
+      Array.from(selectedGenes)
+        .map((selection) => selection.sysName)
+        .includes(allele.sysGeneName)) ||
+    false;
+  const alleleOfVariation =
+    (allele.variationName &&
+      Array.from(selectedVariations)
+        .map((selection) => selection.alleleName)
+        .includes(allele.variationName ?? '')) ||
+    false;
+  const alleleAlreadySelected = Array.from(selectedAlleles)
+    .map((selection) => selection.name)
+    .includes(allele.name);
+  return (alleleOfGene || alleleOfVariation) && !alleleAlreadySelected;
 };
 
-const populateVariationOptions = (
-  setVariationOptions: React.Dispatch<
-    React.SetStateAction<Array<Option<VariationInfo>>>
-  >
-): void => {
-  getVariations()
-    .then((variationRecords) => {
-      const variations = variationRecords.map((record) =>
-        VariationInfo.createFromRecord(record)
-      );
-      const variationOptions: Array<Option<VariationInfo>> = [];
-      for (const variation of variations) {
-        variationOptions.push({ value: variation, label: variation.name });
-      }
-      setVariationOptions(variationOptions);
-    })
-    .catch((e) => {
-      console.error(e);
-      setVariationOptions([]);
-    });
-};
-
-const refreshAlleleOptions = async (
-  selectedGenes: Array<Option<Gene>>,
-  selectedVariations: Array<Option<VariationInfo>>,
-  setAlleleOptions: React.Dispatch<React.SetStateAction<Array<Option<Allele>>>>
-): Promise<void> => {
-  // Build filter
-  const filter: Array<Array<FilterTuple<AlleleFieldName>>> = [];
-  for (const option of selectedGenes) {
-    filter.push([['SysGeneName', { Equal: option.value.sysName }]]);
-  }
-  for (const option of selectedVariations) {
-    filter.push([['VariationName', { Equal: option.value.name }]]);
-  }
-
-  // Note that alleles have to query DB to build rich version
-  try {
-    const alleleRecords = await getFilteredAlleles({
-      filters: filter,
-      orderBy: ['Name'],
-    });
-    const alleles = await Promise.all(
-      alleleRecords.map(async (record) => await Allele.createFromRecord(record))
-    );
-    const alleleOptions: Array<Option<Allele>> = [];
-    for (const allele of alleles) {
-      alleleOptions.push({ value: allele, label: allele.name });
-    }
-
-    setAlleleOptions(alleleOptions);
-  } catch (error) {
-    console.error(error);
-  }
+const SexSelector = (props: {
+  setSelectedSex: (sex: Sex) => void;
+}): JSX.Element => {
+  return (
+    <>
+      <h3 className='font-medium'>Add a New Cross Node</h3>
+      <label className='label'>
+        <span className='label-text'>Sex</span>
+      </label>
+      <select
+        onChange={(event) => {
+          props.setSelectedSex(
+            event.target.value === 'male' ? Sex.Male : Sex.Hermaphrodite
+          );
+        }}
+        className='select-bordered select w-full max-w-xs'
+      >
+        <option value='male'>male</option>
+        <option value='hermaphrodite'>hermaphrodite</option>
+      </select>
+    </>
+  );
 };
 
 const createNewCrossNode = (
@@ -223,7 +209,6 @@ const createNewCrossNode = (
     genes,
     variations,
   };
-  console.log(crossNode);
   return <CrossNode model={crossNode}></CrossNode>;
 };
 
---
+++ src/components/Selector/SelectedPill.tsx
@@ -0,0 +1,43 @@
+import { BiX as CloseIcon } from 'react-icons/bi';
+
+interface SelectedPillProps<U> {
+  removeFromSelected: () => void;
+  displayVal: string;
+}
+
+const SelectedPill = <U,>(props: SelectedPillProps<U>): JSX.Element => {
+  return (
+    <div className='badge-accent badge m-1 p-4 pr-0' key={props.displayVal}>
+      <div className='mr-2'>{props.displayVal}</div>
+      <button
+        onClick={props.removeFromSelected}
+        className='mr-0 h-8 w-8 rounded-full pr-0 align-middle hover:btn-primary'
+      >
+        <CloseIcon className='m-auto text-sm' />
+      </button>
+    </div>
+  );
+};
+
+export const getSelectedPills = <U,>(
+  selectedRecords: Set<U>,
+  removeFromSelected: (value: U) => void,
+  displayResultsOn: Array<keyof U>
+): JSX.Element[] => {
+  const pills = new Array<JSX.Element>();
+  selectedRecords.forEach((record) => {
+    const displayVal = displayResultsOn
+      .map((field) => record[field] as string)
+      .join(', ');
+    pills.push(
+      <SelectedPill
+        removeFromSelected={() => removeFromSelected(record)}
+        displayVal={displayVal}
+      />
+    );
+  });
+
+  return pills;
+};
+
+export default SelectedPill;
