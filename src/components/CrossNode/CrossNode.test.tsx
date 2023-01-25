7b7d3befc52e9086716e1d6d2cf6cf965ae6fdad
Daniel <63615764+danielb3-byte@users.noreply.github.com>
2023-01-25 13:03:33 -0700

    fix: display chromosomes in order in cross nodes

--- src/components/CrossNode/CrossNode.test.tsx
+++ src/components/CrossNode/CrossNode.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import * as mock from 'models/frontend/CrossNode/CrossNode.mock';
-import CrossNode from 'components/CrossNode/CrossNode';
+import CrossNode, { cmpChromosomes } from 'components/CrossNode/CrossNode';
 
 describe('CrossNode component', () => {
   test('Empty node shows no sections', () => {
@@ -28,5 +28,14 @@ describe('CrossNode component', () => {
 
     const chromosomeExSection = screen.getByText(/^Ex$/);
     expect(chromosomeExSection).toBeDefined();
+  });
+
+  test('orders chromosomes', () => {
+    const expected = ['I', 'II', 'III', 'IV', 'V', 'X', 'Ex', undefined];
+    const beforeSort = ['II', 'I', 'X', undefined, 'Ex', 'V', 'IV', 'III'];
+    const afterSort = beforeSort.sort(cmpChromosomes);
+    afterSort.forEach((chrom, idx) => {
+      expect(afterSort[idx]).toEqual(expected[idx]);
+    });
   });
 });
