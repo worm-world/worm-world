419382d36c7d1729ca6ccc9d7742795cad87d683
William Bradshaw <u1253119@utah.edu>
2023-01-30 05:07:08 +0000

    Todo structure and components

---
+++ src/components/Scheduler/components/todoActions.tsx
@@ -0,0 +1,22 @@
+const TodoActions = (props: {}): JSX.Element => {
+  return (
+    <div className='ml-8 mr-8 w-80'>
+      <input
+        type='checkbox'
+        className='checkbox-accent checkbox checkbox-lg absolute top-0 left-0 ml-8'
+      />
+      <div>
+        <div className='grid grid-cols-1 place-items-start gap-4'>
+          <p className='absolute top-0 ml-10 text-3xl'>Jan 19 9:00AM</p>
+          <div className='btn-group absolute bottom-0 mb-4 ml-0'>
+            <label htmlFor='my-modal-3' className='btn'>
+              Postpone
+            </label>
+            <button className='btn-active btn'>Set Complete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default TodoActions;
--- src/pages/scheduler/todo.tsx
+++ src/pages/scheduler/todo.tsx
@@ -1,5 +1,26 @@
+import TodoItem from '../../components/Scheduler/components/todoItem';
+
 const Todo = (): JSX.Element => {
-  return <></>;
+  return (
+    <div className='grid '>
+      <div className='min-w-1000px m-6 '>
+        <p className='text-4xl font-medium'> Current Tasks:</p>
+      </div>
+      {/* Heres where we will map out all the current steps that are specified by the backend */}
+      <div className='min-w-1000px bg-base-300 text-center'>
+        <TodoItem isMarried></TodoItem>
+      </div>
+      <div className='min-w-1000px bg-base-200 text-center'>
+        <TodoItem isSingle></TodoItem>
+      </div>
+      <div className='min-w-1000px bg-base-300 text-center'>
+        <TodoItem shouldFreeze></TodoItem>
+      </div>
+      <div className='min-w-1000px bg-base-200 text-center'>
+        <TodoItem shouldPCR></TodoItem>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
