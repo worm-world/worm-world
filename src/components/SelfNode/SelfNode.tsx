078ea960519863facd5867e1f20fa8f896ad832c
Seth Gunsay <sgunsay1@gmail.com>
2023-02-06 18:23:17 -0700

    feat: toggle sex on editor

--- src/components/SelfNode/SelfNode.tsx
+++ src/components/SelfNode/SelfNode.tsx
@@ -2,7 +2,7 @@ import { TbArrowLoopLeft as SelfIcon } from 'react-icons/tb';
 
 export const SelfNode = (): JSX.Element => {
   return (
-    <div className='h-16 w-16 rounded-full bg-secondary p-4 shadow shadow transition hover:bg-secondary-focus'>
+    <div className='h-16 w-16 rounded-full bg-secondary p-4 shadow transition hover:bg-secondary-focus'>
       <SelfIcon className='h-8 w-8 text-3xl text-primary-content' />
     </div>
   );
